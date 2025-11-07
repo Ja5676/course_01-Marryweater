aa0cc7331abbeecacb36b8ac0ae0beb713b2787d
Disantni4ik <dimasic16252@gmail.com>
2025-11-07 23:02:54 +0200

    First Changes

---
+++ Bot/Program.cs
@@ -0,0 +1,36 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace Bot
+{
+    public class Program
+    {
+        private static TelegramBotClient bot { get; set; }
+        static async Task Main(string[] args)
+        {
+            bot = new TelegramBotClient("7736154319:AAEHu8fE4g8l2bAs_DE1RdeGzrFqSfTlQh8");
+            bot.OnMessage += Bot_OnMessage;
+
+            Console.ReadLine();
+        }
+
+        private static async Task Bot_OnMessage(Message message, UpdateType type)
+        {
+            Console.WriteLine($"{message.Contact?.UserId}: {message.Text}");
+
+            if (message.Text == "/start")
+            {
+                await bot.SendMessage(
+                    chatId: message.Chat.Id,
+                    text: "Запустити WebApp:",
+                    replyMarkup: new InlineKeyboardMarkup(
+                        InlineKeyboardButton.WithWebApp("Відкрити", new WebAppInfo("https://your-site.com"))
+                    )
+                );
+            }
+        }
+    }
+}
+
